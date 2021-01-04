369a409445196703bb842e5c770a283bd66dadc3
Logesh.S <76871611+Everything-with-python@users.noreply.github.com>
2021-01-04 19:15:10 +0530

    Add files via upload

---
+++ pencil art.py
@@ -0,0 +1,11 @@
+import cv2
+import numpy as np
+
+img=cv2.imread("D:\\Python\\py projects\\ironman.jpg")
+gray_img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+edge=cv2.adaptiveThreshold(gray_img,255,
+                           cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,9,9)
+cv2.imshow("img",img)
+cv2.imshow("edge",edge)
+
+
